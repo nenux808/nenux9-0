435be1aa153cb409f42af61566f9d7553ff87b7c
nenux808 <96218006+nenux808@users.noreply.github.com>
2022-10-04 22:25:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,31 +36,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94703604328'] //ur owner number
+global.ownername = "~NENUX9.0~" //ur owner name
+global.ytname = "YT: NENUX BRO" //ur yt chanel name
+global.socialm = "GitHub: nenux808" //ur github or insta name
+global.location = "Sri Lanka, Colombo , NCHS" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
+global.owner = ['94703604328']
+global.ownertag = '94703604328' //ur tag number
+global.botnumber = '94703604328' //Add your Bot Number For Log
+global.botname = 'NENUX9.0 BOT BETA EDITION' //ur bot name
+global.ig = "https://www.youtube.com/c/NenuxzBro" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/c/NenuxzBro" //ur website to be displayed
+global.sc = 'https://github.com/nenux808' //script link
+global.linkz = 'https://www.youtube.com/c/NenuxzBro'
+global.reactmoji = "😎" //ur menu react emoji
+global.themeemoji = "⚡" //ur theme emoji
+global.packname = "NENUX9.0 BOT BETA EDITION" //ur sticker watermark packname
 global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
+global.footer = "NENUX9.0_ᴇᴅɪᴛɪᴏɴ"
+global.aliveimg ="https://i.postimg.cc/FRQZ3mt0/NENUX-BRO-NEW-LOGO-2-ND-EDITION.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.gbyeimg = "https://i.postimg.cc/FRQZ3mt0/NENUX-BRO-NEW-LOGO-2-ND-EDITION.jpg"
+global.alivemsg = "HIII KOLLANE MK WENNE 😎\n@NENUX9.0"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94703604328'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
